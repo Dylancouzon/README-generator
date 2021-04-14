7230cb1c589f669f71344207c773258467675cd2
Dylancouzon <dylancouzon@gmail.com>
2021-04-13 20:46:09 -0700

    add inquirer & fs function

--- index.js
+++ index.js
@@ -4,19 +4,28 @@ const ls = require('ls');
 const markdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = new Map([
-    ['input', 'what is your GitHub adress?', 'gitHuB'], 
-    ['input', 'what is your email adress?', 'email'], 
-    ['input', 'What is your project\'s name ?', 'project'],
-    ['input', 'Please write a description of your project:', 'description'],
-    ['list', 'License:', 'license', ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']]
-]);
+const questions = [
+    { type: 'input', message: 'what is your GitHub adress?', name: 'gitHuB' },
+    { type: 'input', message: 'what is your email adress?', name: 'email' },
+    { type: 'input', message: 'What is your project\'s name ?', name: 'project' },
+    { type: 'input', message: 'Please write a description of your project:', name: 'description' },
+    { type: 'list', message: 'License:', name: 'license', choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'] }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile('log.txt', result, (err) =>
+    err ? console.error(err) : console.log('Success!')
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) =>
+            generateMarkdown(response, writeToFile)
+        );
+}
 
 // Function call to initialize app
 init();
