1e50dbccfeb7b6a357bf876be8ebf515a1f6b955
Dylancouzon <dylancouzon@gmail.com>
2021-04-15 15:59:26 -0700

    style and improved Markdown file creation

--- index.js
+++ index.js
@@ -4,18 +4,25 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 const questions = [
+//     WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
     { type: 'input', message: 'what is your GitHub adress?', name: 'gitHuB' },
     { type: 'input', message: 'what is your email adress?', name: 'email' },
     { type: 'input', message: 'What is your project\'s name ?', name: 'project' },
     { type: 'input', message: 'Please write a description of your project:', name: 'description' },
-    { type: 'list', message: 'License:', name: 'license', choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'] }
+    { type: 'list', message: 'License:', name: 'license', choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'] },
+    { type: 'input', message: 'Installation inscructions:', name: 'install' },
+    { type: 'input', message: 'Utilization instructions:', name: 'usage' },
+    { type: 'input', message: 'How do we test it?', name: 'test' },
+    { type: 'input', message: 'How do we contribute to the project?', name: 'contribute' }
+    
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     // console.log(fileName);
     fs.writeFile(fileName, data, (err) =>
-         err ? console.log('Error!') : console.log('Success!'));
+         err ? console.log('Error!') : console.log('Creating file: '+ fileName));
 }
 
 // TODO: Create a function to initialize app
@@ -24,8 +31,8 @@ function init() {
         .prompt(questions)
         .then((response) => {
             const data = markdown.generateMarkdown(response)
-            console.log(data);
-            writeToFile(response.project + '.md', data);
+            let fileName = response.project.split(" ").join("");
+            writeToFile(fileName + '.md', data);
         }
         );
 }
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-function renderLicense(license) { 
+function renderLicense(license) {
   
   // const licensesList = ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'];
   if(license == "MIT"){
@@ -14,25 +14,70 @@ function renderLicense(license) {
   }
 }
 
-
+function renderLicenseLink(license) {
+  if(license == "MIT"){
+    return 'https://opensource.org/licenses/MIT';
+  }else if(license == "APACHE 2.0"){
+    return 'https://opensource.org/licenses/Apache-2.0';
+  }else if(license == "GPL 3.0"){
+    return 'https://www.gnu.org/licenses/gpl-3.0';
+  }else if(license == "BSD 3"){
+    return 'https://opensource.org/licenses/BSD-3-Clause';
+  }else {
+    return false;
+  }
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
 
   let license = renderLicense(data.license);
+  let licenseLink = renderLicenseLink(data.license);
+  
 let md = `
 # ${data.project}
 
+${license}
+
+## Table of Contents
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Testing](#testing)
+4. [Usage](#usage)
+5. [Contributing](#contributing)
+6. [Author](#author)
+7. [License](#license)
+
+<a name="description"></a>
 ## Description of the project:
 ${data.description}
 
+<a name="installation"></a>
+## Installation instructions
+${data.install}
+
+<a name="testing"></a>
+## Testing
+${data.test}
+
+<a name="usage"></a>
+## How to use it
+${data.usage}
+
+<a name="contributing"></a>
+## How to contribute
+${data.contribute}
+
+<a name="author"></a>
 ## Author's Links
 <${data.email}>
 [GitHub](https://github.com/${data.gitHuB})
+
 `;
-if(license){
+if(licenseLink){
 licenseTxt = `
+<a name="license"></a>
 ## License :
-${license}
+${licenseLink}
 `;
 }
   return md.concat(licenseTxt);
